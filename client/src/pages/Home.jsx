10016e94487d03ed965390e99ba51eabd30c10d5
Ulaş Hasbay <ulashasbay@gmail.com>
2023-03-27 19:12:43 +0300

    backend server changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://dall-e-6qm9.onrender.com/api/v1/post", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
