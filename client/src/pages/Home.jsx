b638e100656ca70728dfab69943a2d820b9a3b32
Ulaş Hasbay <ulashasbay@gmail.com>
2023-03-27 18:22:31 +0300

    integrated backend to frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
 import FormField from "../components/FormField";
 import Loader from "../components/Loader";
@@ -7,17 +7,60 @@ function Home() {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResults = allPosts.filter(
+          (item) =>
+            item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+            item.prompt.toLowerCase().includes(searchText.toLowerCase())
+        );
+        setSearchedResults(searchResults);
+      }, 500)
+    );
+  };
 
   const RenderCards = ({ data, title }) => {
     if (data?.length > 0) {
-        return data.map((post) => <Card key={post._id} {...post}/>)
+      return data.map((post) => <Card key={post._id} {...post} />);
     }
-    
-    return (
-        <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase ">{title}</h2>
-    )
 
-  }
+    return (
+      <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase ">
+        {title}
+      </h2>
+    );
+  };
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -31,7 +74,14 @@ function Home() {
         </p>
       </div>
       <div className="mt-16">
-        <FormField />
+        <FormField
+          labelName="Search posts"
+          type="text"
+          name="text"
+          placeholder="Search posts"
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>
       <div className="mt-10">
         {loading ? (
@@ -42,16 +92,16 @@ function Home() {
           <>
             {searchText && (
               <h2 className="font-medium text-[#666e75] text-xl mb-3">
-                Showing results for <span className="text-[#222328]">{searchText}</span>
+                Showing results for{" "}
+                <span className="text-[#222328]">{searchText}</span>
               </h2>
             )}
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
-                {searchText ? (
-                    <RenderCards data={[]} title="No Results Found" />
-                ) : (
-                    <RenderCards data={[]} title="No Posts Found" />
-                )}
-
+              {searchText ? (
+                <RenderCards data={searchedResults} title="No Results Found" />
+              ) : (
+                <RenderCards data={allPosts} title="No Posts Found" />
+              )}
             </div>
           </>
         )}
